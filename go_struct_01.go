9076d3337264e6fcc497ac47cc2980d5e9dfa51e
rlatks30 <rlatks30@gmail.com>
2020-12-01 15:29:23 +0900

    v1 struct

--- go_struct_01.go
+++ go_struct_01.go
@@ -2,30 +2,46 @@ package main
 
 import "fmt"
 
-type part struct {
-	description string
-	count       int
-}
-
-func showInfo(p part) {
-	fmt.Println("Description :", p.description)
-	fmt.Println("Count : ", p.count)
-}
-
-func minimumOrder(description string) part{
-	var p part
-	p.description = description
-	p.count = 100
-	return p
+type subscriber struct {
+	name   string
+	rate   int
+	active bool
 }
 
 func main() {
-	p := minimumOrder("HEX bolts")
-	fmt.Println(p.description, p.count)
+	var s1 subscriber
 
-//	var bolts part
-//	bolts.description = "HEX bolts"
-//	bolts.count = 100
-//	showInfo(bolts)
+	fmt.Printf("%#v\n", s1)
 
+	s1.name = "kim"
+	s1.rate = 5000
+	s1.active = false
+
+	fmt.Printf("%s\n", s1.name)
+	fmt.Println(s1.rate)
+	fmt.Println(s1.active)
+	/*
+		var subscriber struct {
+						name   string
+									rate   int
+												active bool
+														}
+																fmt.Printf("%#v\n", subscriber)
+																		subscriber.name = "kim"
+																				subscriber.rate = 5000
+																						subscriber.active = false
+																								fmt.Printf("%s\n", subscriber.name)
+																										fmt.Println(subscriber.rate)
+																												fmt.Println(subscriber.active)
+																															subscriber := map[string]bool{}
+																																		subscriber["name"] = "kim"
+																																					subscriber["rate"] = 5000
+																																								subscriber["active"] = false
+																																											fmt.Println("test")
+																																														subscriber := []string{}
+																																																	subscriber = append(subscriber, "kim")
+																																																				subscriber = append(subscriber, 5000)
+																																																							subscriber = append(subscriber, false)
+																																																										fmt.Println("test")
+	*/
 }
